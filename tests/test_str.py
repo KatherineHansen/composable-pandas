d357d10b7b66925b884fa15beb89144e8f3af698
Katherine Hansen <katehansen10@yahoo.com>
2021-02-01 21:25:55 -0600

    still working on

--- tests/test_str.py
+++ tests/test_str.py
@@ -1,9 +1,10 @@
-from composable_pandas.str import capitalize
+from composable_pandas.str import capitalize, get_dummies
 from datetime import datetime
 
 import numpy as np
 import pytest
 
+from pandas import DataFrame, Index, MultiIndex, Series, isna, notna
 from pandas import Series, _testing as tm
 
 def test_capitalize():
@@ -17,3 +18,24 @@ def test_capitalize():
     mixed = mixed >> capitalize()
     exp = Series(["Foo", np.nan, "Bar", np.nan, np.nan, "Blah", np.nan, np.nan, np.nan])
     tm.assert_almost_equal(mixed, exp)
+
+
+def test_get_dummies():
+    s = Series(["a|b", "a|c", np.nan])
+    result = s >> get_dummies("|")
+    expected = DataFrame([[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("abc"))
+    tm.assert_frame_equal(result, expected)
+
+    s = Series(["a;b", "a", 7])
+    result = s >> get_dummies(";")
+    expected = DataFrame([[0, 1, 1], [0, 1, 0], [1, 0, 0]], columns=list("7ab"))
+    tm.assert_frame_equal(result, expected)
+
+    # GH9980, GH8028
+    idx = Index(["a|b", "a|c", "b|c"])
+    result = idx >> get_dummies("|")
+
+    expected = MultiIndex.from_tuples(
+        [(1, 1, 0), (1, 0, 1), (0, 1, 1)], names=("a", "b", "c")
+    )
+    tm.assert_index_equal(result, expected)
