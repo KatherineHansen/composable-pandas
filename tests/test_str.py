b44f9bbc0322a14e10ebfc8c8218813b3eef4828
Katherine Hansen <katehansen10@yahoo.com>
2021-02-02 22:18:25 -0600

    I did it

--- tests/test_str.py
+++ tests/test_str.py
@@ -21,103 +21,30 @@ def test_capitalize():
 
 
 def test_get():
-    # GH 6383
-    s = Series(
-        np.array(
-            [
-                43,
-                48,
-                60,
-                48,
-                50,
-                51,
-                50,
-                45,
-                57,
-                48,
-                56,
-                45,
-                51,
-                39,
-                55,
-                43,
-                54,
-                52,
-                51,
-                54,
-            ]
-        )
-    )
+    values = Series(["a_b_c", "c_d_e", np.nan, "f_g_h"])
 
-    result = s>>get(25, 0)
-    expected = 0
-    assert result == expected
+    result = values.str.split("_") >> get(1)
+    expected = Series(["b", "d", np.nan, "g"])
+    tm.assert_series_equal(result, expected)
 
-    s = Series(
-        np.array(
-            [
-                43,
-                48,
-                60,
-                48,
-                50,
-                51,
-                50,
-                45,
-                57,
-                48,
-                56,
-                45,
-                51,
-                39,
-                55,
-                43,
-                54,
-                52,
-                51,
-                54,
-            ]
-        ),
-        index=pd.Float64Index(
-            [
-                25.0,
-                36.0,
-                49.0,
-                64.0,
-                81.0,
-                100.0,
-                121.0,
-                144.0,
-                169.0,
-                196.0,
-                1225.0,
-                1296.0,
-                1369.0,
-                1444.0,
-                1521.0,
-                1600.0,
-                1681.0,
-                1764.0,
-                1849.0,
-                1936.0,
-            ]
-        ),
-    )
+    # mixed
+    mixed = Series(["a_b_c", np.nan, "c_d_e", True, datetime.today(), None, 1, 2.0])
 
-    result = s>>get(25, 0)
-    expected = 43
-    assert result == expected
+    rs = Series(mixed).str.split("_") >> get(1)
+    xp = Series(["b", np.nan, "d", np.nan, np.nan, np.nan, np.nan, np.nan])
 
-    # GH 7407
-    # with a boolean accessor
-    df = pd.DataFrame({"i": [0] * 3, "b": [False] * 3})
-    vc = df.i.value_counts()
-    result = vc>>get(99, default="Missing")
-    assert result == "Missing"
+    assert isinstance(rs, Series)
+    tm.assert_almost_equal(rs, xp)
 
-    vc = df.b.value_counts()
-    result = vc>>get(False, default="Missing")
-    assert result == 3
+    # bounds testing
+    values = Series(["1_2_3_4_5", "6_7_8_9_10", "11_12"])
 
-    result = vc>>get(True, default="Missing")
-    assert result == "Missing"
+    # positive index
+    result = values.str.split("_") >> get(2)
+    expected = Series(["3", "8", np.nan])
+    tm.assert_series_equal(result, expected)
+
+    # negative index
+    result = values.str.split("_") >> get(-3)
+    expected = Series(["3", "8", np.nan])
+    tm.assert_series_equal(result, expected)
